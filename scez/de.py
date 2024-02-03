b4047fa400187f4871eedacf782a89146062378d
Abolfazl (Abe) <abea@arcinstitute.org>
2024-02-02 16:37:04 -0800

    also improve plot_top_DEG_violinplot

--- scez/de.py
+++ scez/de.py
@@ -51,17 +51,17 @@ def run_deseq(adata, design, n_cpus=8):
     return df
 
 
-def plot_volcano(df, title=None, labels=None, n_genes=False, side='both', font_scale=1, dot_size = 5, **kwargs):
+def plot_volcano(df, title=None, labels=None, n_genes=False, side='both', font_scale=1, dot_size = 5, ax = None, **kwargs):
     dot_size_highlight = dot_size * 1.1
     annotate_font_size = 5 * font_scale
     scatter_font_size = 8 * font_scale
     label_font_size = 9 * font_scale
     title_font_size = 10 * font_scale
 
-    df['name'] = df.index.to_list()
+    if 'name' not in df.columns: df['name'] = df.index.to_list()
     df['-log10(pvalue)'] = - np.log10(df.pvalue)
 
-    fig, ax = plt.subplots(figsize=(3, 3))
+    if not ax: fig, ax = plt.subplots(figsize=(3, 3))
 
     # Scatter plot
     ax.scatter(
@@ -123,15 +123,39 @@ def plot_volcano(df, title=None, labels=None, n_genes=False, side='both', font_s
             ax.annotate(row['name'], (row['log2FoldChange'], row['-log10(pvalue)']), 
                         fontsize=annotate_font_size, ha='right', va='bottom')
 
-    plt.tight_layout()
-    plt.show()
+    if not ax: 
+        plt.tight_layout()
+        plt.show()
 
 
-def plot_top_DEG_violinplot(adata, df):
-    top_genes = df.nlargest(20, '-log10(pvalue)')  # Adjust the number as needed
+def plot_top_DEG_violinplot(adata, df, labels=None, n_genes=False, side='both', font_scale=1, figsize=(10, 4), **kwargs):
+    label_font_size = 9 * font_scale
+    title_font_size = 10 * font_scale
 
-    # Filter the single-cell dataset for the top genes
-    subset_adata = adata[:, top_genes.index]
+    if 'name' not in df.columns: df['name'] = df.index.to_list()
+
+    if labels and n_genes:
+        # error message if both labels and n_genes are provided and say one of them is allowed
+        raise ValueError('Provide either labels or n_genes, not both!')
+
+    if labels:
+        # Highlight the points from given list
+        selected_genes = df.loc[labels]
+
+    elif n_genes and side == 'positive':
+        # Highlight top genes
+        selected_genes = df.query('log2FoldChange > 0').nlargest(n_genes, '-log10(pvalue)')
+
+    elif n_genes and side == 'negative':
+        # Highlight top genes
+        selected_genes = df.query('log2FoldChange < 0').nlargest(n_genes, '-log10(pvalue)')
+
+    elif n_genes and side == 'both':
+        # Highlight top genes
+        selected_genes = df.nlargest(n_genes, '-log10(pvalue)')
+
+    # Filter the single-cell dataset for the selected genes
+    subset_adata = adata[:, selected_genes.index]
     subset_adata.var.index = subset_adata.var.index.str.split('_').str[0]
 
     # Convert the subset of adata to a DataFrame
@@ -144,15 +168,14 @@ def plot_top_DEG_violinplot(adata, df):
     melted_df = pd.melt(merged_df, id_vars='sample', var_name='Gene', value_name='Counts')
 
     # Create a violin plot
-    plt.figure(figsize=(10, 4))
-    sns.violinplot(x='Gene', y='Counts', hue='sample', data=melted_df, split=True, inner='quartile', palette='Set2')
-    sns.stripplot(x='Gene', y='Counts', hue='sample', data=melted_df, dodge=True, jitter=True, color='black', size=1,
-                  alpha=0.3)
+    plt.figure(figsize=figsize)
+    sns.violinplot(x='Gene', y='Counts', hue='sample', data=melted_df, split=True, inner='quartile', palette='Set2', **kwargs)
+    sns.stripplot(x='Gene', y='Counts', hue='sample', data=melted_df, dodge=True, jitter=True, color='black', size=1, alpha=0.3, **kwargs)
 
-    plt.xticks(rotation=45, ha='right', fontsize=8)
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.xticks(rotation=45, ha='right', fontsize=label_font_size)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', fontsize=label_font_size)
 
-    plt.title('Top Differentially Expressed Genes')
+    plt.title('Top Differentially Expressed Genes', fontsize=title_font_size)
     plt.show()
 
 
