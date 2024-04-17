40564d87ddb9c3690b34a5080349ce9c13f922b1
Abolfazl (Abe) <abea@arcinstitute.org>
2024-04-17 15:40:08 -0700

    add use_highly_variable option

--- scez/preprocess.py
+++ scez/preprocess.py
@@ -6,11 +6,14 @@ def normalization(adata):
     adata.layers['raw_counts'] = adata.X.copy()
     sc.pp.normalize_total(adata, target_sum=1e4)
     sc.pp.log1p(adata)
-    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
 
 
-def clustering(adata, lognorm=False, rep=None, n_neighbor=30,n_comps=50,res=None):
-    sc.pp.pca(adata, n_comps=n_comps)
+def clustering(adata, lognorm=False, rep=None, n_neighbor=30,n_comps=50,res=None, use_highly_variable=True):
+    if use_highly_variable:
+        sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+        sc.pp.pca(adata, n_comps=n_comps, use_highly_variable=True)
+    else:
+        sc.pp.pca(adata, n_comps=n_comps)
     sc.pp.neighbors(adata, use_rep=rep, n_neighbors=n_neighbor)
     # why can't we just work with the default neighbors?
     sc.tl.umap(adata)
