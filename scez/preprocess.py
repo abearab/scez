0827276feabb1b6f44064c3f9d3f2beba6c44a0a
Abolfazl (Abe) <abea@arcinstitute.org>
2024-04-18 10:10:33 -0700

    reformat n_pcs argument

--- scez/preprocess.py
+++ scez/preprocess.py
@@ -10,14 +10,14 @@ def normalization(adata,lognorm=True):
 
 
 def clustering(
-        adata, n_comps=50, n_neighbor=30, use_highly_variable=True,
+        adata, n_pcs=50, n_neighbor=30, use_highly_variable=True,
         use_rep=None, resolution=None
         ):
     if use_highly_variable:
         sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
-        sc.pp.pca(adata, n_comps=n_comps, use_highly_variable=True)
+        sc.pp.pca(adata, n_comps=n_pcs, use_highly_variable=True)
     else:
-        sc.pp.pca(adata, n_comps=n_comps)
-    sc.pp.neighbors(adata, use_rep=use_rep, n_neighbors=n_neighbor)
+        sc.pp.pca(adata, n_comps=n_pcs)
+    sc.pp.neighbors(adata, use_rep=use_rep, n_neighbors=n_neighbor, n_pcs=n_pcs)
     sc.tl.umap(adata)
     sc.tl.leiden(adata, resolution=resolution)
