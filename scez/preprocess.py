30d0ffe0785ea9af7c6f8b341267320e9e17a168
Abolfazl (Abe) <abea@arcinstitute.org>
2024-04-18 13:34:07 -0700

    update `normalization` function

--- scez/preprocess.py
+++ scez/preprocess.py
@@ -1,22 +1,23 @@
 import scanpy as sc
 
 
-def normalization(adata,lognorm=True,final_layer='log1p_norm'):
+def normalization(adata, target_sum=1e4, max_value=10, final_layer='scaled'):
     # keep raw counts as a layer
     adata.layers['raw_counts'] = adata.X.copy()
-    scales_counts = sc.pp.normalize_total(adata, target_sum=1e4, inplace=False)
-    if lognorm:
-        # log1p transform
-        adata.layers["log1p_norm"] = sc.pp.log1p(scales_counts["X"], copy=True)
-
+    counts = sc.pp.normalize_total(adata, target_sum=target_sum, inplace=False)
+    # log1p transform
+    adata.layers["log1p_norm"] = sc.pp.log1p(counts["X"], copy=True)
+    # scale counts
+    adata.layers['scaled'] = sc.pp.scale(adata, max_value=max_value, copy=True).X
+    # set the final layer
     adata.X = adata.layers[final_layer]
 
 
 def clustering(
-        adata, n_pcs=50, n_neighbors=30, use_highly_variable=True,
+        adata, n_pcs=50, n_neighbors=30, use_highly_variable='Yes',
         use_rep=None, resolution=None
         ):
-    if use_highly_variable:
+    if use_highly_variable == 'Yes':
         sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
         sc.tl.pca(adata, svd_solver='arpack', use_highly_variable=True)
     else:
