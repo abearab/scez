9ff6a9cfa6eb47e197321875852f3b2d7f522f8b
Abolfazl (Abe) <abea@arcinstitute.org>
2024-04-18 09:53:41 -0700

    move `lognorm` argument

--- scez/preprocess.py
+++ scez/preprocess.py
@@ -1,14 +1,15 @@
 import scanpy as sc
 
 
-def normalization(adata):
+def normalization(adata,lognorm=True):
     # keep raw counts as a layer
     adata.layers['raw_counts'] = adata.X.copy()
     sc.pp.normalize_total(adata, target_sum=1e4)
-    sc.pp.log1p(adata)
+    if lognorm:
+        sc.pp.log1p(adata)
 
 
-def clustering(adata, lognorm=False, rep=None, n_neighbor=30,n_comps=50,res=None, use_highly_variable=True):
+def clustering(adata, rep=None, n_neighbor=30,n_comps=50,res=None, use_highly_variable=True):
     if use_highly_variable:
         sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
         sc.pp.pca(adata, n_comps=n_comps, use_highly_variable=True)
