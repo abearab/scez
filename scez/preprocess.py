27728a18b99a3cb9ac44d03a70ee5d5de7e89a50
Abolfazl (Abe) <abea@arcinstitute.org>
2024-04-19 13:38:41 -0700

    Use `adjustText` to fix label overlap in volcano plots Fixes #10

--- scez/preprocess.py
+++ scez/preprocess.py
@@ -14,14 +14,17 @@ def normalization(adata, target_sum=1e4, max_value=10, final_layer='scaled'):
 
 
 def clustering(
-        adata, n_pcs=50, n_neighbors=30, use_highly_variable='Yes',
-        use_rep=None, resolution=None
+        adata
         ):
-    if use_highly_variable == 'Yes':
-        sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
-        sc.tl.pca(adata, svd_solver='arpack', use_highly_variable=True)
-    else:
-        sc.pp.pca(adata, n_comps=n_pcs)
-    sc.pp.neighbors(adata, use_rep=use_rep, n_neighbors=n_neighbors)#, n_pcs=n_pcs)
-    sc.tl.umap(adata)
-    sc.tl.leiden(adata, resolution=resolution)
+    pass
+    # , n_pcs=50, n_neighbors=30, use_highly_variable='Yes',
+    #     use_rep=None, resolution=None
+    
+    # if use_highly_variable == 'Yes':
+    #     sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+    #     sc.tl.pca(adata, svd_solver='arpack', use_highly_variable=True)
+    # else:
+    #     sc.pp.pca(adata, n_comps=n_pcs)
+    # sc.pp.neighbors(adata, use_rep=use_rep, n_neighbors=n_neighbors)#, n_pcs=n_pcs)
+    # sc.tl.umap(adata)
+    # sc.tl.leiden(adata, resolution=resolution)
