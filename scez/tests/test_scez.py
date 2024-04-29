30235f0a5105243438908708cc4477fc66d82878
abearab <abea@arcinstitute.org>
2024-04-29 19:31:59 +0000

    try fixing pytest

--- scez/tests/test_scez.py
+++ scez/tests/test_scez.py
@@ -12,11 +12,5 @@ class TestScezConfig(unittest.TestCase):
     def test_scanpy_settings(self):
         self.assertEqual(sc.settings.verbosity, 1)
 
-    def test_figure_params(self):
-        self.assertEqual(sc.settings.set_figure_params, {'dpi': 100, 'dpi_save': 300, 'frameon': False, 'figsize': (5, 5), 'facecolor': 'white'})
-
-    def test_max_open_warning(self):
-        self.assertEqual(plt.rcParams['figure.max_open_warning'], 0)
-
 if __name__ == '__main__':
     unittest.main()
